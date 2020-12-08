fac72903dfa5b51d48993137b18aa1b443af313c
Markus Stubinger <markus.stubinger@gmail.com>
2020-12-08 15:09:43 +0100

    server additions - RegistrationController, updateController

--- src/main/kotlin/at/mstubinger/familyplanner/service/controller/AppointmentsController.kt
+++ src/main/kotlin/at/mstubinger/familyplanner/service/controller/AppointmentsController.kt
@@ -2,6 +2,7 @@ package at.mstubinger.familyplanner.service.controller
 
 import at.mstubinger.familyplanner.service.data.Appointment
 import at.mstubinger.familyplanner.service.data.LoggedInUser
+import at.mstubinger.familyplanner.service.utils.DataBaseActionsHelper
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -12,20 +13,30 @@ import javax.servlet.http.HttpServletResponse
 @RestController
 class AppointmentsController {
 
+    private val dbah = DataBaseActionsHelper()
 
-    @PostMapping("/appointments/update")
-    fun login(
+    @PostMapping("/appointments/getAll")
+    fun getAllAppointments(
             @RequestParam("uid") userId: String,
             response: HttpServletResponse,
             request: HttpServletRequest
-    ) {
+    ): ArrayList<Appointment> {
 
-        val appointments:ArrayList<Appointment> = ArrayList()
-
-        val query = "SELECT * FROM APPOINTMENTS " +
-                "WHERE APPTOWNER_ID='$userId'"
+        return dbah.getAllAppointments(userId)
 
     }
 
+    @PostMapping("appointments/create")
+
+    fun createAppoinemtent(
+            @RequestParam("uid") userId: String,
+            
+            response: HttpServletResponse,
+            request: HttpServletRequest
+    ): ArrayList<Appointment> {
+
+        return dbah.getAllAppointments(userId)
+
+    }
 
 }
--- src/main/kotlin/at/mstubinger/familyplanner/service/utils/DataBaseActionsHelper.kt
+++ src/main/kotlin/at/mstubinger/familyplanner/service/utils/DataBaseActionsHelper.kt
@@ -1,18 +1,44 @@
 package at.mstubinger.familyplanner.service.utils
 
 import at.mstubinger.familyplanner.service.data.Appointment
+import java.time.format.DateTimeFormatter
 
 class DataBaseActionsHelper {
 
     private val dbch = DataBaseConnectionHelper()
 
 
-    fun getAllAppointments (userId:String){
+    fun getAllAppointments (userId:String): ArrayList<Appointment> {
 
         val appointments:ArrayList<Appointment> = ArrayList()
 
-        val query = "SELECT * FROM"
+        val query = "SELECT * FROM APPOINTMENTS" +
+                "WHERE APPTOWNER_ID='$userId'"
 
+
+        return appointments
+
+    }
+
+    fun createAppointment(userId: String, appointment:Appointment){
+
+        var apptType=1
+
+        when(appointment.type!!.toLowerCase()){
+            "garbage" ->  apptType = 3
+            "birthday" -> apptType = 2
+            else -> apptType = 1
+        }
+
+        val isFullDayAppt = 0
+
+        val startDate = appointment.startDate.date.toString()
+        val endDate = appointment.endDate.date.toString()
+
+        val query = "INSERT INTO APPOINTMENTS (APPTOWNER_ID, TITLE, LOCATION, TYPE, REOCCURENCE, START, END, ISFULLDAYAPPT)" +
+                "VALUES ($userId, '${appointment.title}', '${appointment.location}',$apptType , '${appointment.reoccurance}', '$startDate', '$endDate', $isFullDayAppt)"
+
+        dbch.update(query)
 
     }
 
